69a3b19de114110ac71f2ae979761ca5ad79751f
Mikhail Fursov <mike.fursov@gmail.com>
2019-06-04 16:48:55 -0400

    Wrapping song to 2 or 3 columns if there is enough space on the screen

--- app/components/song-content/song-view.component.ts
+++ app/components/song-content/song-view.component.ts
@@ -1,10 +1,14 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, Input, OnChanges, OnDestroy, OnInit, PLATFORM_ID, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, Inject, Input, OnChanges, OnDestroy, OnInit, PLATFORM_ID, SimpleChanges} from '@angular/core';
 import {SongDetails} from '@common/artist-model';
 import {Observable, Subject} from 'rxjs';
 import {map, takeUntil} from 'rxjs/operators';
 import {UserDataService} from '@app/services/user-data.service';
 import {isPlatformBrowser} from '@angular/common';
 import {renderChords} from '@app/utils/chords-renderer';
+
+/** Heuristic used to enable multicolumn mode. */
+const MIN_SONG_LEN_FOR_2_COLUMN_MODE = 800;
+const MIN_SONG_LEN_FOR_3_COLUMN_MODE = 1200;
 
 /** Отображает содежимое мести (текст) без заголовка и прочей мета-информации. */
 @Component({
@@ -18,6 +22,7 @@ export class SongViewComponent implements OnInit, OnChanges, OnDestroy {
   readonly destroyed$ = new Subject<unknown>();
 
   @Input() song!: SongDetails;
+  @Input() multiColumnMode = true;
   songHtml: string = '';
 
   userSongStyle$!: Observable<{ [key: string]: string; }>;
@@ -26,6 +31,7 @@ export class SongViewComponent implements OnInit, OnChanges, OnDestroy {
   b4Si?: boolean;
 
   readonly isBrowser: boolean;
+  private availableWidth = 0;
 
   constructor(private readonly cd: ChangeDetectorRef,
               private readonly uds: UserDataService,
@@ -60,6 +66,8 @@ export class SongViewComponent implements OnInit, OnChanges, OnDestroy {
           this.updateSongView();
           this.cd.detectChanges();
         });
+
+    this.updateAvailableWidth();
   }
 
   ngOnDestroy(): void {
@@ -72,5 +80,24 @@ export class SongViewComponent implements OnInit, OnChanges, OnDestroy {
 
   private updateSongView(): void {
     this.songHtml = this.song && this.isBrowser ? renderChords(this.song.content, {tag: 'c', transpose: this.transpose, useH: !this.b4Si}) : '';
+  }
+
+  @HostListener('window:resize', [])
+  onWindowResize() {
+    this.updateAvailableWidth();
+  }
+
+  private updateAvailableWidth(): void {
+    if (this.multiColumnMode) {
+      this.availableWidth = window.innerWidth;
+    }
+  }
+
+  is2ColumnMode(): boolean {
+    return this.song.content.length > MIN_SONG_LEN_FOR_2_COLUMN_MODE && this.availableWidth > 1300 && !this.is3ColumnMode();
+  }
+
+  is3ColumnMode(): boolean {
+    return this.song.content.length > MIN_SONG_LEN_FOR_3_COLUMN_MODE && this.availableWidth > 1920;
   }
 }
