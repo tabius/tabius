66ebd9a003b4a46f4b2c9a3e48076a7e5c5178a5
Mikhail Fursov <mike.fursov@gmail.com>
2019-08-04 20:33:34 -0400

    Better handling of 401 error code

--- app/interceptors/errors.interceptor.ts
+++ app/interceptors/errors.interceptor.ts
@@ -1,18 +1,27 @@
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable, throwError} from 'rxjs';
-import {FORUM_LOGIN_LINK} from '@common/mounts';
-import {catchError} from 'rxjs/operators';
+import {catchError, flatMap, take} from 'rxjs/operators';
+import {AuthService} from '@app/services/auth.service';
+import {fromPromise} from 'rxjs/internal-compatibility';
+
+const HTTP_STATUS_UNAUTHORIZED = 401;
 
 @Injectable()
 export class ErrorsInterceptor implements HttpInterceptor {
+
+  constructor(private readonly authService: AuthService,) {
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
         .pipe(
             catchError((error: HttpErrorResponse) => {
-              if (error.status === 401) {
-                window.location.href = FORUM_LOGIN_LINK;
+              if (error.status === HTTP_STATUS_UNAUTHORIZED) {
+                return fromPromise(this.authService.signOut()).pipe(
+                    take(1),
+                    flatMap(() => throwError(error))
+                );
               }
               return throwError(error);
             })
