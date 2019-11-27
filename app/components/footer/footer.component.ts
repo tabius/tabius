9c909987c6c4324edfbca323d5bf0697c249a428
Mikhail Fursov <mike.fursov@gmail.com>
2019-11-26 20:05:10 -0500

    Making footer to work as a link

--- app/components/footer/footer.component.ts
+++ app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {environment} from '@app/environments/environment';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'gt-footer',
@@ -8,7 +9,11 @@ import {environment} from '@app/environments/environment';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent {
-  version = new Date(environment.buildInfo.buildDate).toISOString().split('T')[0].replace(/-/g, '').substring(2);
+
+  readonly version = new Date(environment.buildInfo.buildDate).toISOString().split('T')[0].replace(/-/g, '').substring(2);
+
+  constructor(readonly router: Router) {
+  }
 
   getRandomizedFooterClass(): string {
     return 'c' + (1 + Date.now() % 5);
