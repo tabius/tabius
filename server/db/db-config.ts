3dc8b7763e29c8f00a905b29f150262847dbe2df
Mikhail Fursov <mike.fursov@gmail.com>
2019-12-08 15:03:38 -0500

    Fixing DB config adjustment code

--- server/db/db-config.ts
+++ server/db/db-config.ts
@@ -12,7 +12,7 @@ export function readDbConfig() {
   const configPath = process.env.TABIUS_DB_CONFIG_PATH;
   if (configPath) {
     const dbConfigAdjustment = require(configPath);
-    return {...DEFAULT_DB_CONFIG, dbConfigAdjustment};
+    return {...DEFAULT_DB_CONFIG, ...dbConfigAdjustment};
   }
   return DEFAULT_DB_CONFIG;
 }
