2f3d76036ed5dc9f79bc013708210273c19b1260
Mikhail Fursov <mike.fursov@gmail.com>
2019-12-08 14:55:48 -0500

    Logging current DB details on startup

--- server/db/db.service.ts
+++ server/db/db.service.ts
@@ -1,14 +1,17 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {createPool, Pool} from 'mysql2';
 import {readDbConfig} from '@server/db/db-config';
 
 @Injectable()
 export class DbService {
 
+  private readonly logger = new Logger(DbService.name);
+
   public pool: Pool;
 
   constructor() {
     const dbConfig = readDbConfig();
+    this.logger.log(`Connecting to database: ${dbConfig.database}`);
     this.pool = createPool(dbConfig);
   }
 
