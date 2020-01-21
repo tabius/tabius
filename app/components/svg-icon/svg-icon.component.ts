46c84d8ae3bfeaa76b125ae63bc0b57d4e09df0e
Mikhail Fursov <mike.fursov@gmail.com>
2020-01-20 21:34:06 -0500

    Adding build timestampt to svg assets bundle to force reload on updates.

--- app/components/svg-icon/svg-icon.component.ts
+++ app/components/svg-icon/svg-icon.component.ts
@@ -1,14 +1,17 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {environment} from '@app/environments/environment';
 
 @Component({
   selector: 'gt-svg-icon',
   styleUrls: ['./svg-icon.component.scss'],
   template: `
       <svg>
-          <use [attr.xlink:href]="'/assets/symbol-defs.svg#' + icon"></use>
+          <use [attr.xlink:href]="'/assets/symbol-defs.svg?'+version+'#' + icon"></use>
       </svg>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SvgIconComponent {
   @Input() icon!: string;
+
+  readonly version = environment.buildInfo.buildDate;
 }
