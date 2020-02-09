4452f756329780e583e92499fe5c9f78078bf81c
Mikhail Fursov <mike.fursov@gmail.com>
2020-02-09 14:12:08 -0500

    Working with domino library directly with no external template

--- server/server-main.module.ts
+++ server/server-main.module.ts
@@ -1,6 +1,7 @@
 import {Logger, Module} from '@nestjs/common';
 import {join} from 'path';
-import {AngularUniversalModule, applyDomino} from '@nestjs/ng-universal';
+import {AngularUniversalModule} from '@nestjs/ng-universal';
+import * as domino from 'domino';
 
 import {ServerDbModule} from './db/server-db.module';
 import {UserController} from './controller/user.controller';
@@ -9,13 +10,12 @@ import {CollectionController} from './controller/collection.controller';
 import {APP_INTERCEPTOR} from '@nestjs/core';
 import {ServerSsoService} from '@server/service/server-sso.service';
 
-const BROWSER_DIR = join(process.cwd(), 'dist/browser');
-applyDomino(global, join(BROWSER_DIR, 'index.html'));
+applyDomino();
 
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
-      viewsPath: BROWSER_DIR,
+      viewsPath: join(process.cwd(), 'dist/browser'),
       bundle: require('./../dist/server/main.js'),
     }),
     ServerDbModule,
@@ -32,3 +32,25 @@ applyDomino(global, join(BROWSER_DIR, 'index.html'));
 })
 export class ServerMainModule {
 }
+
+
+function applyDomino(): void {
+  const win = domino.createWindow('<body></body>');
+
+  global['window'] = win;
+  Object.defineProperty(
+      win.document.body.style,
+      'transform',
+      {
+        value: () => ({
+          enumerable: true,
+          configurable: true,
+        })
+      },
+  );
+  global['document'] = win.document;
+  global['navigator'] = win.navigator;
+  global['CSS'] = null;
+  global['Prism'] = null;
+}
+
