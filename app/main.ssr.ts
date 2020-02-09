7fd86da43e32eee8ec5f749f0673013a87164713
Mikhail Fursov <mike.fursov@gmail.com>
2020-02-09 14:17:32 -0500

    Forcing prod mode in Angular server side code

--- app/main.ssr.ts
+++ app/main.ssr.ts
@@ -1,9 +1,9 @@
 import {enableProdMode} from '@angular/core';
-import {environment} from '@app/environments/environment';
+// import {environment} from '@app/environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
+// if (environment.production) {
+enableProdMode();
+// }
 
 export {ngExpressEngine} from '@nguniversal/express-engine';
 export {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
