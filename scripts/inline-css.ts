9e8a69a36d5d2eabd97588b4064bdf010ba13939
Mikhail Fursov <mike.fursov@gmail.com>
2020-02-15 20:23:42 -0500

    Inlining global styles.css into index.html file

---
+++ scripts/inline-css.ts
@@ -0,0 +1,10 @@
+import {readdirSync, readFileSync, writeFileSync} from 'fs';
+
+const fileList = readdirSync('./dist/browser');
+const cssFileName = fileList.find(name => name.startsWith('styles.') && name.endsWith('.css'));
+const htmlFilePath = './dist/browser/index.html';
+const htmlFile = readFileSync(htmlFilePath).toString();
+const cssFile = readFileSync(`./dist/browser/${cssFileName}`).toString();
+const htmlFileWithCss = htmlFile.replace(`<link rel="stylesheet" href="${cssFileName}">`, `<style>${cssFile}</style>`);
+
+writeFileSync(htmlFilePath, htmlFileWithCss);
