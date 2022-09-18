858e2823b993721ec67e16e922c0d534e6fa019a
Mikhail Fursov <mike.fursov@gmail.com>
2022-09-18 12:56:33 -0500

    Upgrade NestJS to v9

--- server/db/full-text-search-dbi.service.ts
+++ server/db/full-text-search-dbi.service.ts
@@ -1,9 +1,10 @@
-import {HttpService, Injectable, Logger} from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {FullTextSongSearchResult, MAX_FULL_TEXT_SEARCH_CONTENT_RESULTS, MAX_FULL_TEXT_SEARCH_TITLE_RESULTS} from '@common/ajax-model';
 import {MIN_LEN_FOR_FULL_TEXT_SEARCH} from '@common/common-constants';
 import {toSafeSearchText} from '@common/util/misc-utils';
 import {SERVER_CONFIG} from '@server/server-config';
 import {firstValueFrom} from 'rxjs';
+import {HttpService} from '@nestjs/axios';
 
 const SPHINX_SQL_URL = 'http://localhost:9307/sql';
 
--- server/db/server-db.module.ts
+++ server/db/server-db.module.ts
@@ -1,10 +1,11 @@
-import {HttpModule, Module} from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {UserDbi} from './user-dbi.service';
 import {SongDbi} from './song-dbi.service';
 import {DbService} from './db.service';
 import {CollectionDbi} from './collection-dbi.service';
 import {FullTextSearchDbi} from '@server/db/full-text-search-dbi.service';
 import {ServerAuthService} from '@server/service/server-auth.service';
+import {HttpModule} from '@nestjs/axios';
 
 @Module({
   imports: [
