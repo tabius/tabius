704181b8feb394215aadb44123a0ae5677c1c0c8
Mikhail Fursov <mike.fursov@gmail.com>
2022-09-20 01:07:56 -0500

    Enable requests tracing with Sentry

--- app/main.ts
+++ app/main.ts
@@ -3,18 +3,13 @@ import {AppModule} from '@app/app.module';
 import {enableProdMode} from '@angular/core';
 import {environment} from '@app/environments/environment';
 import * as Sentry from '@sentry/angular';
+import {BrowserTracing} from '@sentry/tracing';
 
 if (environment.production) {
   Sentry.init({
     dsn: 'https://fc6fba35084549b6a9fdf8843dc74887@o1134925.ingest.sentry.io/6755287',
-    integrations: [
-      // new BrowserTracing({
-      //   tracingOrigins: ['localhost', 'https://tabius.ru/api'],
-      //   routingInstrumentation: Sentry.routingInstrumentation,
-      // }),
-    ],
-    tracesSampleRate: 0,
-    sendClientReports: false,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 0.2,
   });
 
   enableProdMode();
