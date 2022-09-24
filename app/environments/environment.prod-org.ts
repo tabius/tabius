31a16493a0644c16dddbb352399acbf56a0f5a3c
Mikhail Fursov <mike.fursov@gmail.com>
2022-09-24 15:32:17 -0500

    Update tabius.org environment

--- app/environments/environment.prod-org.ts
+++ app/environments/environment.prod-org.ts
@@ -3,6 +3,8 @@ import {TabiusEnv} from '@app/environments';
 
 const APP_DOMAIN = 'tabius.org';
 
+const backendUrl = `https://${APP_DOMAIN}`;
+
 export const environment: TabiusEnv = {
   lang: 'en',
   domain: APP_DOMAIN,
@@ -10,11 +12,26 @@ export const environment: TabiusEnv = {
   production: true,
   backendUrl: `https://${APP_DOMAIN}`,
   ssrBackendUrl: 'http://localhost:24001',
-  telegramUrl: 'https://t.me/tabiusru',
+  telegramUrl: 'https://t.me/tabiusru', // TODO:
   buildInfo,
   authConfig: {
-    // TODO:
-    'domain': 'tabius-org.us.auth0.com',
-    'clientId': 'HsyF6TwzMaKVFhjlLdlBUJf3gs9oihFt',
+    domain: 'tabius-org.us.auth0.com',
+    clientId: 'elgYidwkWEROavQBMADZdDUKQCtoVIYC',
+    cacheLocation: 'localstorage',
+    audience: 'web-client-api-id',
+    httpInterceptor: {
+      allowedList: [
+        {
+          uri: backendUrl + '/api/*',
+          allowAnonymous: true,
+          tokenOptions: {
+            /** The attached token should target this audience. */
+            audience: 'web-client-api-id',
+            /** The attached token should have these scopes. */
+            scope: 'read:current_user'
+          }
+        }
+      ]
+    }
   }
 };
