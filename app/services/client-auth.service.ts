fef156f642705d226f41181799275869d91142bb
Mikhail Fursov <mike.fursov@gmail.com>
2022-10-01 15:20:55 -0500

    Restore original page after sign-in

--- app/services/client-auth.service.ts
+++ app/services/client-auth.service.ts
@@ -5,18 +5,31 @@ import {Observable, of} from 'rxjs';
 import {User} from 'auth0';
 import {environment} from '@app/environments/environment';
 import {truthy} from '@common/util/misc-utils';
+import {Router} from '@angular/router';
+
+/** State of the application when signin() method is called. */
+interface AppStateOnSignIn {
+  pathname: string;
+}
 
 @Injectable({providedIn: 'root'})
 export class ClientAuthService {
 
-  private readonly auth0Service?: AuthService;
+  private readonly auth0Service?: AuthService<AppStateOnSignIn>;
 
   constructor(
       injector: Injector,
       browserStateService: BrowserStateService,
+      router: Router,
   ) {
     if (browserStateService.isBrowser) {
       this.auth0Service = injector.get(AuthService);
+      this.auth0Service.appState$.subscribe(state => {
+        console.debug('ClientAuthService.auth0Service.appState$', state);
+        if (state?.pathname && state.pathname !== window.location.pathname) {
+          setTimeout(async () => await router.navigateByUrl(state.pathname, {replaceUrl: true}), 500);
+        }
+      });
     }
   }
 
@@ -36,6 +49,9 @@ export class ClientAuthService {
       audience: environment.authConfig.audience,
       prompt: 'select_account',
       screen_hint: 'login',
+      appState: <AppStateOnSignIn>{
+        pathname: window.location.pathname
+      }
     });
   }
 
