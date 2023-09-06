734de7cd70a433175d0824802039c70eb7c30bdd
Mikhail Fursov <mike.fursov@gmail.com>
2023-09-05 21:51:05 -0500

    Tests for all envs

--- tests/pw-tests/catalog-page.spec.ts
+++ tests/pw-tests/catalog-page.spec.ts
@@ -6,8 +6,6 @@ const {rootUrl, i18n, collections} = TEST_ENV;
 test('has expected content', async ({page}) => {
   await page.goto(`${rootUrl}/catalog`);
   await expect(page).toHaveTitle(i18n.catalogPage.meta.title);
-  await expect(page.getByTestId('loading-indicator')).toBeVisible();
-  // Several seconds after.
-  await expect(page.getByTestId('loaded-content')).toBeVisible();
+  await expect(page.getByTestId('loaded-content')).toBeVisible({timeout: 15_000});
   await expect(page.locator('.letter-collections').getByTestId(`${collections[0].id}`)).toBeVisible();
 });
--- tests/pw-tests/tests.env.ts
+++ tests/pw-tests/tests.env.ts
@@ -1,19 +1,6 @@
-import {I18N} from '@app/app-i18n';
-import {I18n} from '@common/i18n/i18n';
+import {getI18n, I18n} from '@common/i18n/i18n';
 import {DeepReadonly} from '@common/typescript-extras';
 
-
-export const TEST_ENV: PlaywrightTestEnvironment = {
-  rootUrl: 'http://localhost:12102',
-  i18n: I18N,
-  collections: [
-    {
-      id: 1531,
-      mount: 'iowa',
-      name: 'IOWA',
-      songs: [{id: 92445, mount: 'bet-bit', name: 'Бьет бит'}],
-    }],
-};
 
 interface PlaywrightTestEnvironment {
   rootUrl: string;
@@ -32,3 +19,51 @@ interface TestCollectionData {
     name: string;
   }];
 }
+
+const TEST_ENV_DEV_RU: PlaywrightTestEnvironment = {
+  rootUrl: 'http://localhost:12102',
+  i18n: getI18n('ru'),
+  collections: [
+    {
+      id: 1531,
+      mount: 'iowa',
+      name: 'IOWA',
+      songs: [{id: 92445, mount: 'bet-bit', name: 'Бьет бит'}],
+    }],
+};
+
+const TEST_ENV_PROD_RU: PlaywrightTestEnvironment = {...TEST_ENV_DEV_RU, rootUrl: 'https://tabius.ru'};
+
+const TEST_ENV_DEV_EN: PlaywrightTestEnvironment = {
+  rootUrl: 'http://localhost:13102',
+  i18n: getI18n('org'),
+  collections: [
+    {
+      id: 4,
+      mount: 'abba',
+      name: 'ABBA',
+      songs: [{id: 60, mount: 'andante-andante', name: 'Andante Andante'}],
+    }],
+};
+
+const TEST_ENV_PROD_ORG: PlaywrightTestEnvironment = {...TEST_ENV_DEV_RU, rootUrl: 'https://tabius.org'};
+
+let selectedTestEnv: PlaywrightTestEnvironment;
+switch (process.env['TEST_ENV']) {
+  case 'org':
+    selectedTestEnv = TEST_ENV_PROD_ORG;
+    break;
+  case 'ru':
+    selectedTestEnv = TEST_ENV_PROD_RU;
+    break;
+  case 'dev-org':
+    selectedTestEnv = TEST_ENV_DEV_EN;
+    break;
+  case 'dev-ru':
+  default:
+    selectedTestEnv = TEST_ENV_DEV_RU;
+    break;
+}
+
+export const TEST_ENV = selectedTestEnv;
+
