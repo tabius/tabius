32bc562799afef5d9082e37d7381df3200472c52
Mikhail Fursov <mike.fursov@gmail.com>
2024-01-06 01:22:15 -0600

    Remove .angular dir too

--- etc/pm2/tabius-ru/rebuild.sh
+++ etc/pm2/tabius-ru/rebuild.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# Change to the desired directory
+# Change to the desired directory.
 cd ~/work/tabius-ru || { echo "Failed to change directory to ~/work/tabius-ru"; exit 1; }
 
-# Pull the latest changes from Git
+# Pull the latest changes from Git.
 git pull --verbose || { echo "Git pull failed"; exit 1; }
 
-# Remove node_modules and install dependencies
+# Remove node_modules and build dirs and install dependencies.
+rm -rf dist || { echo "Failed to remove dist directory"; exit 1; }
+rm -rf .angular || { echo "Failed to remove .angular directory"; exit 1; }
 rm -rf node_modules || { echo "Failed to remove node_modules"; exit 1; }
 npm install || { echo "npm install failed"; exit 1; }
 
-# Stop the pm2 process
+# Stop the pm2 process.
 pm2 stop tabius-ru nest-ru || { echo "pm2 stop failed"; exit 1; }
 
-# Remove the dist directory and build
-rm -rf dist || { echo "Failed to remove dist directory"; exit 1; }
+# Rebuild.
 npm run build:ssr-ru || { echo "npm run build:ssr-ru failed"; exit 1; }
 
-# Start the pm2 process
+# Start the pm2 process.
 pm2 start tabius-ru nest-ru || { echo "pm2 start failed"; exit 1; }
 
 echo "Tabius redeployed successfully."
