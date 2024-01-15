f4490d174cbbadef1ff082d0cb90358e2a9c9c51
Mikhail Fursov <mike.fursov@gmail.com>
2024-01-14 18:03:00 -0600

    Fix usage of HttpClient in SSR mode

--- app/app.config.server.ts
+++ app/app.config.server.ts
@@ -2,9 +2,14 @@ import { ApplicationConfig, importProvidersFrom, mergeApplicationConfig } from '
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from '@app/app.config';
 import { AppModule } from '@app/app.module';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), importProvidersFrom(AppModule)],
+  providers: [
+    provideServerRendering(),
+    importProvidersFrom(AppModule),
+    provideHttpClient(withFetch()),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
