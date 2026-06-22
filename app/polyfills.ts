66cf10c106a2b7cf81a00a88ec51801586abfbd7
Mikhail Fursov <mike.fursov@gmail.com>
2026-06-22 18:39:08 -0500

    Remove polyfills

--- app/polyfills.ts
+++
@@ -1,5 +0,0 @@
-// Zoneless: zone.js is no longer used (zoneless change detection is the Angular default).
-// TODO: incompatible with hydration
-//  import * as svg4everybody from 'svg4everybody';
-//
-// svg4everybody({polyfill: true});
